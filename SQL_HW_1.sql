dfc85a235fdc0067178a56d2c47af8b88a414430
AleYuk <uhnoaleksander@gmail.com>
2022-02-17 22:05:35 +0300

    done

--- SQL_HW_1.sql
+++ SQL_HW_1.sql
@@ -20,83 +20,83 @@ select name, email from students
 select id, name, email, created_on from students
 
 --8. ¬ывести пользователей где password 12333
-select id, name, password  from students
+select * from students
 where password = '12333' 
 
 --9. ¬ывести пользователей которые были созданы 2021-03-26 00:00:00
-select id, name, created_on from students
+select * from students
 where created_on = '2021-03-26 00:00:00'
 
 --10. ¬ывести пользователей где в имени есть слово јнна
-select id, name from students
+select * from students
 where name like '%Anna%'
 
 --11. ¬ывести пользователей где в имени в конце есть 8
-select id, name from students
+select * from students
 where name like '%8'
 
 --12. ¬ывести пользователей где в имени в есть буква а
-select id, name from students
+select * from students
 where name like '%a%'
 
 --13. ¬ывести пользователей которые были созданы 2021-07-12 00:00:00
-select id, name, created_on from students
+select * from students
 where created_on = '2021-07-12 00:00:00'
 
 --14. ¬ывести пользователей которые были созданы 2021-07-12 00:00:00 и имеют пароль 1m313
-select id, name, created_on, password from students
+select * from students
 where created_on = '2021-07-12 00:00:00' and password = '1m313'
 
 --15. ¬ывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть слово Andrey
-select id, name, created_on from students
+select * from students
 where created_on = '2021-07-12 00:00:00' and name like '%Andrey%'
 
 --16. ¬ывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть цифра 8
-select id, name, created_on from students
+select * from students
 where created_on = '2021-07-12 00:00:00' and name like '%8%'
 
 --17. ¬ывести пользовател€ у которых id равен 110
-select id, name from students
+select * from students
 where id = '110'
 
 --18. ¬ывести пользовател€ у которых id равен 153
-select id, name from students
+select * from students
 where id = '153'
 
 --19. ¬ывести пользовател€ у которых id больше 140
-select id, name from students
+select * from students
 where id > '140'
 
 --20. ¬ывести пользовател€ у которых id меньше 130
-select id, name from students
+select * from students
 where id < '130'
 
 --21. ¬ывести пользовател€ у которых id меньше 127 или больше 188
-select id, name from students
+select * from students
 where id < '127' or id > '188'
 
 --22. ¬ывести пользовател€ у которых id меньше либо равно 137
-select id, name from students
+select * from students
 where id <= '137'
 
 --23. ¬ывести пользовател€ у которых id больше либо равно 137
-select id, name from students
+select * from students
 where id >= '137'
 
 --24. ¬ывести пользовател€ у которых id больше 180 но меньше 190
-select id, name from students
+select * from students
 where id > '180' and id < '190'
 
 --25. ¬ывести пользовател€ у которых id между 180 и 190
-select id, name from students
+select * from students
 where id between 180 and 190
 
 --26. ¬ывести пользователей где password равен 12333, 1m313, 123313
-select id, name, password from students
+select * from students
 where password in ('12333', '1m313', '123313')
 
 --27. ¬ывести пользователей где created_on равен 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00
-select id, name, created_on from students
+select * from students
 where created_on in ('2020-10-03 00:00:00', '2021-05-19 00:00:00', '2021-03-26 00:00:00')
 
 --28. ¬ывести минимальный id 
